ddbd73713dcc1df679a87748f5b0ba9ff0099212
Emad Karamad <emad.karamad@madetech.com>
2021-10-29 17:52:57 +0100

    Add top exceptions and automation report types

--- src/@bichard/types/src/AuditLog.ts
+++ src/@bichard/types/src/AuditLog.ts
@@ -1,9 +1,13 @@
 import type AuditLogEvent from "./AuditLogEvent"
+import type AutomationReport from "./AutomationReport";
+import type TopExceptionsReport from "./TopExceptionsReport";
 
 export default interface AuditLog {
   messageId: string
   caseId: string
   events: AuditLogEvent[]
+  automationReport: AutomationReport
+  topExceptionsReport: TopExceptionsReport
   status: string
   lastEventType: string
   version: number
