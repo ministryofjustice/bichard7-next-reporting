4408efe9f7083fe0048133aa97f69853219cd1f1
Emad Karamad <emad.karamad@madetech.com>
2021-11-04 11:33:03 +0000

    Update forces module for new forces list format

---
+++ src/@bichard/forces/src/forces.ts
@@ -0,0 +1,55 @@
+const forces = `
+XX	Stoplist
+01	London MET
+02	London MET
+03	Cumbria
+04	Lancashire
+05	Merseyside
+06	Greater Manchester
+07	Cheshire
+10	Northumbria
+11	Durham
+12	North Yorkshire
+13	West Yorkshire
+14	South Yorkshire
+15	HM Forces and other global include
+16	Humberside
+17	Cleveland
+20	West Midlands
+21	Staffordshire
+22	West Mercia
+23	Warwickshire
+24	MoD
+30	Derbyshire
+31	Nottinghamshire
+32	Lincolnshire
+33	Leicestershire
+34	Northamptonshire
+35	Cambridgeshire
+36	Norfolk
+37	Suffolk
+40	Bedfordshire
+41	Hertfordshire
+42	Essex
+43	Thames Valley
+44	Hampshire
+45	Surrey
+46	Kent
+47	Sussex
+48	City of London
+50	Devon and Cornwall
+52	Avon and Somerset
+53	Gloucestershire
+54	Wiltshire
+55	Dorset
+60	North Wales
+61	Gwent
+62	South Wales
+63	Dyfed-Powys
+73	HMCS global include
+88	HMRC
+89	ACRO
+91	NCA
+93	BTP`
+
+export default forces
