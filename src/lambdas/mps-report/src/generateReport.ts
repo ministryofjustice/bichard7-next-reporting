4cb5541dd7447815e588cf10d16aff4446438c8b
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-12-15 11:03:17 +0000

    Updating with the things

--- src/lambdas/mps-report/src/generateReport.ts
+++ src/lambdas/mps-report/src/generateReport.ts
@@ -54,19 +54,58 @@ export default async (gateway: PostgresGateway) => {
   result.push([""])
   result.push(headers)
   for (let i = 0; i < rows.length; i = i + 1) {
-    const annotatedMsg: string = rows[i].annotated_msg.replace(/br7:/g, "").replace(/ds:/g, "")
-    let annotatedMsgObject = (xml2js(annotatedMsg, { compact: true }) as AnnotatedPNCUpdateDataset)?.AnnotatedPNCUpdateDataset?.PNCUpdateDataset
-    if(!annotatedMsgObject) {
-      annotatedMsgObject = (xml2js(annotatedMsg, { compact: true }) as AnnotatedHearingOutcome)
-    }
-    const offences = annotatedMsgObject.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
-    const hearingOutcomeCase = annotatedMsgObject.AnnotatedHearingOutcome.HearingOutcome.Case
+    if(new Date(rows[i].court_date) < new Date()){
+      const annotatedMsg: string = rows[i].annotated_msg.replace(/br7:/g, "").replace(/ds:/g, "")
+      let annotatedMsgObject = (xml2js(annotatedMsg, { compact: true }) as AnnotatedPNCUpdateDataset)?.AnnotatedPNCUpdateDataset?.PNCUpdateDataset
+      if(!annotatedMsgObject) {
+        annotatedMsgObject = (xml2js(annotatedMsg, { compact: true }) as AnnotatedHearingOutcome)
+      }
+      const offences = annotatedMsgObject.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
+      const hearingOutcomeCase = annotatedMsgObject.AnnotatedHearingOutcome.HearingOutcome.Case
 
-    if (isMultiple<OffenceDetails>(offences)) {
-      for (let o = 0; o < offences.length; o = o + 1) {
-        const newRow = updateCommonHeaders(rows[i], hearingOutcomeCase, offences[o])
+      if (isMultiple<OffenceDetails>(offences)) {
+        for (let o = 0; o < offences.length; o = o + 1) {
+          const newRow = updateCommonHeaders(rows[i], hearingOutcomeCase, offences[o])
 
-        const results = offences[o].Result
+          const results = offences[o].Result
+          if (isMultiple<OffenceResult>(results)) {
+            for (let j = 0; j < 10; j = j + 1) {
+              if (j >= results.length) {
+                newRow.push("") // result code
+                newRow.push("") // result duration/amount
+                newRow.push("") // result text
+                newRow.push("") // result qualifier
+              } else {
+                // j-th
+                newRow.push(getText(results[j].CJSresultCode)) // result code
+                newRow.push(getDurationAndAmount(results[j])) // result duration/amount
+                newRow.push(getText(results[j].ResultVariableText)) // result text
+                newRow.push(getResultQualifierCodes(results[j].ResultQualifierVariable)) // result qualifier
+              }
+            }
+          } else {
+            newRow.push(getText(results.CJSresultCode)) // result code
+            newRow.push(getDurationAndAmount(results)) // result duration/amount
+            newRow.push(getText(results.ResultVariableText)) // result text
+            newRow.push(getResultQualifierCodes(results.ResultQualifierVariable)) // result qualifier
+
+            // fill in the remaining 9 groups of cells
+            for (let j = 0; j < 9; j = j + 1) {
+              newRow.push("") // result code
+              newRow.push("") // result duration/amount
+              newRow.push("") // result text
+              newRow.push("") // result qualifier
+            }
+          }
+          newRow.push(rows[i].trigger_locked_by_id ? rows[i].trigger_locked_by_id : "") // Trigger Locked By
+          newRow.push(rows[i].error_locked_by_id ? rows[i].error_locked_by_id : "") // Exception Locked By
+
+          result.push(newRow)
+        }
+      } else {
+        const newRow = updateCommonHeaders(rows[i], hearingOutcomeCase, offences)
+
+        const results = offences.Result
         if (isMultiple<OffenceResult>(results)) {
           for (let j = 0; j < 10; j = j + 1) {
             if (j >= results.length) {
@@ -101,43 +140,6 @@ export default async (gateway: PostgresGateway) => {
 
         result.push(newRow)
       }
-    } else {
-      const newRow = updateCommonHeaders(rows[i], hearingOutcomeCase, offences)
-
-      const results = offences.Result
-      if (isMultiple<OffenceResult>(results)) {
-        for (let j = 0; j < 10; j = j + 1) {
-          if (j >= results.length) {
-            newRow.push("") // result code
-            newRow.push("") // result duration/amount
-            newRow.push("") // result text
-            newRow.push("") // result qualifier
-          } else {
-            // j-th
-            newRow.push(getText(results[j].CJSresultCode)) // result code
-            newRow.push(getDurationAndAmount(results[j])) // result duration/amount
-            newRow.push(getText(results[j].ResultVariableText)) // result text
-            newRow.push(getResultQualifierCodes(results[j].ResultQualifierVariable)) // result qualifier
-          }
-        }
-      } else {
-        newRow.push(getText(results.CJSresultCode)) // result code
-        newRow.push(getDurationAndAmount(results)) // result duration/amount
-        newRow.push(getText(results.ResultVariableText)) // result text
-        newRow.push(getResultQualifierCodes(results.ResultQualifierVariable)) // result qualifier
-
-        // fill in the remaining 9 groups of cells
-        for (let j = 0; j < 9; j = j + 1) {
-          newRow.push("") // result code
-          newRow.push("") // result duration/amount
-          newRow.push("") // result text
-          newRow.push("") // result qualifier
-        }
-      }
-      newRow.push(rows[i].trigger_locked_by_id ? rows[i].trigger_locked_by_id : "") // Trigger Locked By
-      newRow.push(rows[i].error_locked_by_id ? rows[i].error_locked_by_id : "") // Exception Locked By
-
-      result.push(newRow)
     }
   }
 
