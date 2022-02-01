78f0ede9b9ae31d76fd5c18add6f747a10628847
Ben Pirt <ben@pirt.co.uk>
2022-02-01 16:40:41 +0000

    Fix errors caused after upgrade

--- src/lambdas/common-platform-report/src/generateDates.test.ts
+++ src/lambdas/common-platform-report/src/generateDates.test.ts
@@ -4,7 +4,7 @@ describe("generateDates", () => {
   it("should generate the correct times", () => {
     const now = new Date("2021-12-21T18:50:26.123Z")
     const result = generateDates(now, 12)
-    expect(result.end.toISOString()).toEqual("2021-12-21T18:00:00.000Z")
-    expect(result.start.toISOString()).toEqual("2021-12-21T06:00:00.000Z")
+    expect(result.end.toISOString()).toBe("2021-12-21T18:00:00.000Z")
+    expect(result.start.toISOString()).toBe("2021-12-21T06:00:00.000Z")
   })
 })
