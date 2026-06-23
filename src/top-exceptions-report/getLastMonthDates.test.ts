7a234a3b38d601cf02fc1a9173fe2e7452bb8aab
Richard Race <1273965+richrace@users.noreply.github.com>
2026-06-23 12:27:38 +0100

    Revert to the existing code

--- src/top-exceptions-report/getLastMonthDates.test.ts
+++ src/top-exceptions-report/getLastMonthDates.test.ts
@@ -5,20 +5,20 @@ describe("getLastMonthDates", () => {
     const now = new Date("2021-12-21T18:50:26.123Z")
     const result = getLastMonthDates(now)
     expect(result.start.toISOString()).toBe("2021-11-01T00:00:00.000Z")
-    expect(result.end.toISOString()).toBe("2021-11-30T23:59:59.999Z")
+    expect(result.end.toISOString()).toBe("2021-12-01T00:00:00.000Z")
   })
 
-  it("should generate the correct dates for Jan", () => {
-    const now = new Date("2021-02-21T18:50:26.123Z")
+  it("should generate the correct dates for January", () => {
+    const now = new Date("2021-01-21T18:50:26.123Z")
     const result = getLastMonthDates(now)
-    expect(result.start.toISOString()).toBe("2021-01-01T00:00:00.000Z")
-    expect(result.end.toISOString()).toBe("2021-01-31T23:59:59.999Z")
+    expect(result.start.toISOString()).toBe("2020-12-01T00:00:00.000Z")
+    expect(result.end.toISOString()).toBe("2021-01-01T00:00:00.000Z")
   })
 
   it("should generate the handle dates for BST", () => {
     const now = new Date("2021-07-21T18:50:26.123Z")
     const result = getLastMonthDates(now)
     expect(result.start.toISOString()).toBe("2021-05-31T23:00:00.000Z")
-    expect(result.end.toISOString()).toBe("2021-06-30T22:59:59.999Z")
+    expect(result.end.toISOString()).toBe("2021-06-30T23:00:00.000Z")
   })
 })
--- src/top-exceptions-report/getLastMonthDates.ts
+++ src/top-exceptions-report/getLastMonthDates.ts
@@ -1,21 +1,10 @@
-import { endOfMonth, startOfMonth, subMonths } from "date-fns"
-import { toZonedTime, toDate } from "date-fns-tz"
-
 export type TimeRange = {
   start: Date
   end: Date
 }
 
 export default (now: Date): TimeRange => {
-  const timeZone = "Europe/London"
-
-  const zonedNow = toZonedTime(now, timeZone)
-
-  const zonedStart = startOfMonth(subMonths(zonedNow, 1))
-  const zonedEnd = endOfMonth(subMonths(zonedNow, 1))
-
-  return {
-    start: toDate(zonedStart, { timeZone }),
-    end: toDate(zonedEnd, { timeZone })
-  }
+  const end = new Date(now.getFullYear(), now.getMonth())
+  const start = new Date(now.getFullYear(), now.getMonth() - 1)
+  return { start, end }
 }
