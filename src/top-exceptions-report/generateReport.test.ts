51d07a8883a3c5fb3dd6f252bca1a7759c025b5e
Richard Race <1273965+richrace@users.noreply.github.com>
2026-06-23 09:41:09 +0100

    Add date range title to xlsx

--- src/top-exceptions-report/generateReport.test.ts
+++ src/top-exceptions-report/generateReport.test.ts
@@ -1,8 +1,15 @@
 import generateReport from "./generateReport"
 import messages from "./test/dummyMessages"
+import type { TimeRange } from "./getLastMonthDates"
+import { subMonths } from "date-fns"
 
 it("should generate the correct report", () => {
-  const report = generateReport(messages(new Date("2021-10-14T14:08:00.000Z")))
+  const timeRange: TimeRange = {
+    start: subMonths(new Date("2021-10-14T14:08:00.000Z"), 1),
+    end: new Date("2021-10-14T14:08:00.000Z")
+  }
+
+  const report = generateReport(messages(new Date("2021-10-14T14:08:00.000Z")), timeRange)
 
   expect(report.toString()).toMatchSnapshot()
 })
