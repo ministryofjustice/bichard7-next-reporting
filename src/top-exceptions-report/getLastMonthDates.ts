bf6becfc6b5fe4a15df82d03d1508b2d081a1cb5
Richard Race <1273965+richrace@users.noreply.github.com>
2026-06-23 09:39:59 +0100

    Add better date handling

--- src/top-exceptions-report/getLastMonthDates.ts
+++ src/top-exceptions-report/getLastMonthDates.ts
@@ -1,10 +1,13 @@
+import { endOfMonth, startOfMonth, subMonths } from "date-fns"
+
 export type TimeRange = {
   start: Date
   end: Date
 }
 
 export default (now: Date): TimeRange => {
-  const end = new Date(now.getFullYear(), now.getMonth())
-  const start = new Date(now.getFullYear(), now.getMonth() - 1)
+  const start = startOfMonth(subMonths(now, 1))
+  const end = endOfMonth(subMonths(now, 1))
+
   return { start, end }
 }
